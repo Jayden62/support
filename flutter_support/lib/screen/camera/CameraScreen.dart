e5a0222e1aa836eb49f26d9a9187bf8998d0fd6e
lenghia <letrungnghia621997@gmail.com>
2019-10-29 17:53:33 +0700

    commit msg

--- flutter_support/lib/screen/camera/CameraScreen.dart
+++ flutter_support/lib/screen/camera/CameraScreen.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_support/style/Style.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -13,6 +11,36 @@ class CameraScreen extends StatefulWidget {
 class CameraState extends State<CameraScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.photo_camera),
+              Container(
+                margin: EdgeInsets.only(left: margin10),
+                child: Text('Camera takes one'),
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+            child: Container(
+          margin: EdgeInsets.only(top: margin10, left: margin10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.add_a_photo),
+              Container(
+                margin: EdgeInsets.only(left: margin10),
+                child: Text('Camera takes many'),
+              ),
+            ],
+          ),
+        )),
+      ],
+    );
   }
 }
